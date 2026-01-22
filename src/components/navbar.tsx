7c50389ed9e392e17c659649760c1df819ece60e
rrxzyy <reikun1207@gmail.com>
2026-01-22 13:27:16 +0700

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <nav className="flex flex-col gap-3 bg-white p-6 text-neutral-900 border-b border-neutral-300 drop-shadow">
       <h1 className="flex flex-row gap-3 items-center justify-start text-center md:text-3xl text-2xl font-bold">
         <span>
-          <img src="src/assets/react.svg" alt="react logo" className="w-6 h-6" />
+          <img src="/react.svg" alt="react logo" className="w-6 h-6" />
         </span>
         Volunteer Event
       </h1>
